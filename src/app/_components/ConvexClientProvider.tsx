bb690ab2c1f0adaaf9f0d26fea273034637b454e
Anusha Chitranshi <chitraaa@mail.uc.edu>
2024-08-24 14:59:48 -0400

    feat(client): added initial clerk files

---
+++ src/app/_components/ConvexClientProvider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import type { ReactNode } from "react";
+
+
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+export default function ConvexClientProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+    >
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+}
